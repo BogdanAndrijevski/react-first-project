f26da0647d00cdf91904ef2af6a639b060f59f3a
unknown <bogdanlikuliks@hotmail.com>
2020-01-22 18:37:33 +0100

    Splitting an App Into Components - tut 86

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from '../components/Persons/Person/Person';
+// import Person from '../components/Persons/Person/Person';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
 
 
 
@@ -49,24 +51,18 @@ class App extends Component {
   render() {
     let persons = null;
     // console.log('firs ', btnClass);
-    let btnClass = '';
-    console.log(btnClass.toString());
+    // let btnClass = '';
+    // console.log(btnClass.toString());
 
     if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key={person.id}
-              changed={(event) => this.nameChangedHandler(event, person.id)} />
-          })}
-        </div>
-      );
+      persons = 
+          <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+          />;
 
-      btnClass = classes.Red;
+      // btnClass = classes.Red;
 
       // style.backgroundColor = 'red';
       // style[':hover'] = {
@@ -77,25 +73,16 @@ class App extends Component {
 
     // let classes = ['red', 'bold'].join(' '); // "red bold" - what we get here
 
-    let assignedClasses = [];
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red); //classes = ['red']
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
-    }
+    
 
     return (
       
     <div className={classes.App}>
-      <h1>Hi, I'm a React App</h1>
-      <p className={assignedClasses.join(' ')}>This is really working!!!</p>
-      <button
-        // alt={this.state.showPersons}
-        className={btnClass}
-        onClick={this.togglePersonsHandler}>
-        Toggle Persons
-        </button>
+      <Cockpit
+      showPersons={this.setState.showPersons}
+      persons={this.state.persons}
+      clicked={this.togglePersonsHandler}
+      />
       {persons}
     </div>
 
