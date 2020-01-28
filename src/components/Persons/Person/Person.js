04748fc76765a5a5f6717709c947513197b6d2a3
unknown <bogdanlikuliks@hotmail.com>
2020-01-28 04:17:48 +0100

    91. Component Update LifeCycle (for props Changes)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,26 +1,20 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import classes from './Person.css';
 
 
-const person = (props) => {
+// const person = (props) => {
+class Person extends Component {
+    render(){
     console.log('[person.js] rendering...');
-
-    // const style = {
-    //     '@media (min-width: 500px)': {
-    //         width: '450px',
-    //         color: 'lime'
-    //     }
-    // };
     return (
-        // <div className="Person" style={style}>
         <div className={classes.Person}>
-            < p onClick={props.click} > I'm {props.name} and I am {props.age} years old!</p>
-            < p > {props.children}</p >
-            <input type="text" onChange={props.changed} value={props.name} />
+            < p onClick={this.props.click} > I'm {this.props.name} and I am {this.props.age} years old!</p>
+            < p > {this.props.children}</p >
+            <input type="text" onChange={this.props.changed} value={this.props.name} />
         </div>
     )
-};
+}};
 
 
-export default person;
+export default Person;
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -25,9 +25,9 @@ class App extends Component {
     console.log('[App.js] getDerivedStateFromProps', props);
     return state;
   }
-  componentWillMount(){
-    console.log('[App.js] componentWillMount');
-  }
+  // componentWillMount(){
+  //   console.log('[App.js] componentWillMount');
+  // }
   componentDidMount(){
     console.log('[App.js] componentDidMount');
   }
